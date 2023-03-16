7650132d252b7d3d3cf094b55460f41c79c63d7b
krishna <sahkrishna1126@gmail.com>
2023-03-16 17:29:33 +0530

    code

--- java.js
+++ java.js
@@ -15,6 +15,11 @@ function closeCoupon(){
     document.getElementById('demo').style.opacity=1;
 }
 
+const wrapper =document.querySelector('.wrapper');
+const loginLink =document.querySelector('.login-link');
+const registerLink =document.querySelector('.register-link');
+registerLink.addEventListener('click',()=> {wrapper.classList.add('active');})
+loginLink.addEventListener('click',()=> {wrapper.classList.remove('active');})
 
 // function loadcoupon(){document.getElementById('coupon').style.visibility='visible';
 //   document.getElementsByClassName('dark-mode').style.opacity=0.7}
